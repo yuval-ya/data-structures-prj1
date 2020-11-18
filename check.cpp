ff403ae8c9a663675b5ff51be3abf1032d37c4e6
yuval <yakovskind@gmail.com>
2020-11-18 14:06:03 +0200

    deleted files

--- check.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  check.cpp
-//  data-structures-prj1
-//
-//  Created by Yuval  Yakovskind  on 18/11/2020.
-//  Copyright © 2020 Tamir Houri 205668627 & Yuval Yakovskind 316550524. All rights reserved.
-//
-
-#include "check.h"
