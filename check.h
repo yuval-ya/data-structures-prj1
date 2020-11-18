25ff19c6f073865a2814d119168031bc4454baf5
yuval <yakovskind@gmail.com>
2020-11-18 13:23:54 +0200

    check

--- check.h
+++ check.h
@@ -1,14 +1,9 @@
 //
-//  check.hpp
+//  check.h
 //  data-structures-prj1
 //
 //  Created by Yuval  Yakovskind  on 18/11/2020.
 //  Copyright © 2020 Tamir Houri 205668627 & Yuval Yakovskind 316550524. All rights reserved.
 //
 
-#ifndef check_hpp
-#define check_hpp
-
-#include <stdio.h>
-
-#endif /* check_hpp */
+#pragma once
